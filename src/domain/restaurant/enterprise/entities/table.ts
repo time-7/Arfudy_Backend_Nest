f367ab0bdd32b02b4e22c3ab6330cccd12eb8195
Adrian Silva <adriansilva296@gmail.com>
2023-09-29 19:31:58 -0300

    table get and set

--- src/domain/restaurant/enterprise/entities/table.ts
+++ src/domain/restaurant/enterprise/entities/table.ts
@@ -1,5 +1,28 @@
 import { Entity } from '@core/entities/entity';
 
-export interface TableProps {}
+export interface TableProps {
+  tableNum: number;
+  activeToken: string;
+  seatNum: number;
+}
 
-export class Table extends Entity<TableProps> {}
+export class Table extends Entity<TableProps> {
+  get tableNum(): number {
+    return this.props.tableNum;
+  }
+  set tableNum(tableNum: number) {
+    this.props.tableNum = tableNum;
+  }
+  get activeToken(): string {
+    return this.props.activeToken;
+  }
+  set activeToken(activeToken: string) {
+    this.props.activeToken = activeToken;
+  }
+  get seatNum(): number {
+    return this.props.seatNum;
+  }
+  set seatNum(seatNum: number) {
+    this.props.seatNum = seatNum;
+  }
+}
